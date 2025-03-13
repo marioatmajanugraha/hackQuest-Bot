87c7afcdec5983fd8cb81a5f1dce92275bca5c26
marioatmajanugraha <mario.vodrec@gmail.com>
2025-03-13 09:14:25 +0700

    first commit

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+title hackQuest Daily Check-in
+echo Memulai bot...
+node index.obf.js
+echo Bot telah berhenti.
+timeout /t 3 >nul
+echo Tekan tombol apa saja untuk keluar...
+pause >nul
